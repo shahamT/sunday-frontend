1b56568e7eaaca4b67ecd20442f438cbd703f1ce
Dana Ziv <dana@ziv.co>
2025-05-13 19:34:34 +0300

    Create filter cmp

---
+++ src/cmps/app/main/board/value-setter/AddNewSelection.jsx
@@ -0,0 +1,33 @@
+// === Libs
+
+// === Services
+
+// === Actions
+import { addGroup } from "../../../../../store/actions/board.actions.js"
+
+// === Hooks / React
+
+// === Imgs
+
+// === Child Components
+
+// ====== Component ======
+// =======================
+
+export function AddNewSelection({ onCloseModal }) {
+    // === Consts
+
+    // === Effects
+
+    // === Functions
+    function onAddGroup() {
+        addGroup()
+    }
+
+    // if (!data) return <div>Loading...</div>
+    return (
+        <section className="AddNewSelection">
+            <div className="___-btn clickable clear size-32 icon-start i-Group" onClick={onAddGroup}>New group of items</div>
+        </section>
+    )
+}
