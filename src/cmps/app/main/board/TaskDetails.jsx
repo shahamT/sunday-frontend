115b0c1ce133da14c4edad1223a63ddd890ec5e7
Dana Ziv <dana@ziv.co>
2025-05-14 14:00:28 +0300

    Add title to task details

--- src/cmps/app/main/board/TaskDetails.jsx
+++ src/cmps/app/main/board/TaskDetails.jsx
@@ -1,12 +1,19 @@
 // === Libs
 
 // === Services
+import { showErrorMsg } from "../../../../services/base/event-bus.service"
+import { setColumnValue } from "../../../../store/actions/board.actions.js"
 
 // === Actions
 
 // === Hooks / React
+import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router'
 import { useParams } from 'react-router-dom'
+import { useSelector } from "react-redux"
+import { EditableText } from '../../../../cmps/reusables/EditableText/EditableText.jsx'
+import { useControlledInput } from '../../../../hooks/useControlledInput.js'
+
 
 // === Imgs
 
@@ -15,22 +22,67 @@ import { useParams } from 'react-router-dom'
 // ====== Component ======
 // =======================
 
-export function TaskDetails({ /* prop1, prop2 */ }) {
+export function TaskDetails() {
     // === Consts
     const navigate = useNavigate()
-    const {boardId} = useParams()
-
+    const {boardId, taskId} = useParams()
+    const board = useSelector(storeState => storeState.boardModule.board)
+    
+    const [task, setTask] = useState(null)
+    const [value, handleChange, reset, set] = useControlledInput('')
+    
     // === Effects
+    useEffect(() => {
+        
+        const foundTask = getTaskById(taskId)
+        if (foundTask) {
+            setTask(foundTask)
+            set(foundTask.columnValues[0]?.value || '')
+        }
+    }, [board, taskId])
 
     // === Functions
     function onCloseTaskDetails() {
         navigate(`/app/board/${boardId}`)
     }
 
-    // if (!data) return <div>Loading...</div>
+    function onSetName() {
+        if (!value.trim()) {
+            showErrorMsg(`Task name can't be empty`)
+            set(task?.columnValues[0]?.value || '')
+            return
+        }
+
+        try {
+            setColumnValue(taskId, task?.columnValues[0]?.colId, value)
+            //set task?
+        }
+        catch (err) {
+            showErrorMsg(`Somthing went wrong`)
+        }
+    }
+
+    function getTaskById(taskId) {
+        
+        for (const group of board?.groups || []) {
+            for (const task of group.tasks) {
+                if (task.id === taskId) return task
+            }
+        }
+        return null
+    }
+
+    if (!task) return <div>Loading...</div>
     return (
         <section className="TaskDetails">
-            <p>TaskDetails</p>
+            <EditableText
+                    value={value}
+                    full={true}
+                    size="title"
+                    handleChange={handleChange}
+                    onBlur={onSetName}
+                    onPressEnter={onSetName}
+                />
             <button onClick={onCloseTaskDetails}>X</button>
         </section>
     )
