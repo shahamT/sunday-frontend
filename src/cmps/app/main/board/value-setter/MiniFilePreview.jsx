5f45566d83b78f6a421c976de034534f811ffc3d
Noga Laufer <nogalaufer1@gmail.com>
2025-05-15 12:00:02 +0300

    commit

---
+++ src/cmps/app/main/board/value-setter/MiniFilePreview.jsx
@@ -0,0 +1,29 @@
+// === Libs
+
+// === Services
+
+// === Actions
+
+// === Hooks / React
+
+// === Imgs
+
+// === Child Components
+
+// ====== Component ======
+// =======================
+
+export function MiniFilePreview({ /* prop1, prop2 */ }) {
+    // === Consts
+
+    // === Effects
+
+    // === Functions
+
+    // if (!data) return <div>Loading...</div>
+    return (
+        <section className="mini-file-preview">
+            <h1>ComponentName</h1>
+        </section>
+    )
+}
--- src/pages/DevPages/DevPage2.jsx
+++ src/pages/DevPages/DevPage2.jsx
@@ -4,6 +4,7 @@
 
 import { useState } from "react";
 import { PopUpMenu } from "../../cmps/reusables/PopUpMenu/PopUpMenu";
+import { MiniFilePreview } from "../../cmps/app/main/board/value-setter/MiniFilePreview";
 
 // === Services
 
@@ -22,24 +23,25 @@ export function DevPage2({ /* prop1, prop2 */ }) {
 
 
 
-    
+    const imgUrl = "https://img.freepik.com/premium-photo/playful-cute-fish-photo_960396-928043.jpg"
     return (
         <section className='color-picker-btn'>
             <h1>dev2</h1>
-            {/* <PopUpMenu
+            <PopUpMenu
                 position="start-end"
                 renderContent={({ onCloseModal }) => (
-                    <ColorPicker
+                    <MiniFilePreview
                         onCloseModal={onCloseModal}
-                        selectedColor={selectedColor}
-                        setColor={setColor}
-                        variant={variant}
+                        imgUrl={imgUrl}
+                        // selectedColor={selectedColor}
+                        // setColor={setColor}
+                        // variant={variant}
                     />
                 )}
             >
                 <div className={`color-picker-btn ${selectedColor}-bg`} />
 
-            </PopUpMenu>  */}
+            </PopUpMenu> 
         </section>
     )
 }
