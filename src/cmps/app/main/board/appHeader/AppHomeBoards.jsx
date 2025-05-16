f8928e8e895f6216b2a4952a56aa52d00d4a3b87
Noga Laufer <nogalaufer1@gmail.com>
2025-05-16 12:36:35 +0300

    appHome pic

--- src/cmps/app/main/board/appHeader/AppHomeBoards.jsx
+++ src/cmps/app/main/board/appHeader/AppHomeBoards.jsx
@@ -33,13 +33,11 @@ export function AppHomeBoards({ /* prop1, prop2 */ }) {
             {boards.length > 0 ? boards.map(board =>
                 <div key={board._id} className="board-preview" onClick={() => navigate(`/app/board/${board._id}`)}>
                     <div className="img-wrapper">
-                        <img src="https://res.cloudinary.com/ditvgrfxq/image/upload/v1747320341/kkvc6y2yzsy9taa4d7y2.png" alt="Monday board" />
+                        <img src="https://res.cloudinary.com/ditvgrfxq/image/upload/v1747385287/dd8krocnjoffk6ioxbqb.png" alt="Monday board" />
                         {/* <img src={mondayLogo} alt="Monday board" /> */}
                     </div>
                     <div className="board-txt-container clickable select size-32 icon-start full-width i-Board">
-                        <div className="board-txt "
-
-                        >
+                        <div className="board-txt ">
                             {board.name}</div>
 
                         <button
